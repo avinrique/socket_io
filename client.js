6a23f1b980f958fafd976bad1d56186ed33cd899
avviinrique <avigupta2001ad@gmail.com>
2024-04-09 03:43:12 +0530

    added README.md file

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const { io } = require("socket.io-client");
-const socket = io("http://172.16.6.144:3001");
+const socket = io("http://localhost:3001");
 
 socket.on("connect", () => {
   console.log("Connected with socket id:", socket.id);
